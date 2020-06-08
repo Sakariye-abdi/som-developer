635f9afd361f0f7ba81ed448f3d3c6c6d9f3ce95
Sakariye-abdi <66298737+Sakariye-abdi@users.noreply.github.com>
2020-06-08 14:42:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,263 @@
-<?php include("header.php"); ?>
 
-<?php include("content.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="Viewport" content="width=device-width, intial-scale=1.0">
+	<meta http-equiv="X-UI-Compatible" content="ie-edge">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;800&display=swap" rel="stylesheet">
+	<title>Shuuriye</title>
+    
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="tabs_menu.css">
+	<script src="jquery-3.5.1.min.js"></script>
+    <script src="script.js"></script>
+</head>
+<body>
+  <header>
+      <div class="social-icons">
+		  <ul>
+            <li><a href=""><i class="fa fa-facebook"></i></a></li>
+		    <li><a href=""><i class="fa fa-twitter"></i></a></li>
+		    <li><a href=""><i class="fa fa-instagram"></i></a></li>
+	      </ul>
+	</div>
+      <div class="nav-container">
+         <div class="logo">
+			 <img src="images/logo.png" alt="Logo">
+		 </div>
 
-<?php include("footer.php"); ?>
+		 <nav class="main">
+			 <ul>
+				 <li><a href="">Shuuriye</a></li>
+				 <li><a href="">Abwaanada</a></li>
+				 <li><a href="">Barnaamijyada</a></li>
+			 </ul>
+		 </nav>
+
+		 <div class="btn">
+			 <a href="">Nala xariir</a>
+			 <span class="fa fa-search"></span>
+		 </div>
+	  </div>
+  </header>
+
+
+
+
+
+
+<section class="section1">
+        <div class="section1-container">
+            <div class="box taageer">
+                <h1>TAAGEER</h1>
+                <div class="taageer-image">
+
+                    <div class="transparent">
+                        <p>$10.0 <span>FUNDED</span></p>
+                        <p>$80.0 <span>OURS</span></p>
+                    </div>
+
+                    
+                </div>
+
+                <div class="progressing">
+                       <div class="green"></div>
+                </div>
+
+                <div class="text">
+                    <p>waa xarun dhaqan iyo suugaan tii ugu
+                        horraysay oo laga dhisayo magaalada
+                        dusmo reeb, caasimadda galmudug
+                    </p>
+                </div>
+
+                <div class="taageer-btn">
+                    <a href="">TAAGEER HADDA<i class="fa fa-chevron-right"></i></a>
+                </div>
+            </div>
+
+            <div class="box wargayska">
+                <h1>WARGAYSKA GOBOLADA</h1>
+                <div class="wargayska-image">
+                    <h3>Bille ka soo baxa
+                        magaalada Dusmo Mareb
+                    </h3>
+                    <a href=""><i class="fa fa-chevron-right"></i>HALKAN KA AKHRISO</a>
+                </div>
+            </div>
+
+            <div class="box jadwal">
+                <h1>JADWALKA</h1>
+                <div class="habeen">
+                    <p>BISHA MAARSO 2019</p>
+                    <div class="habeenka-abwaanka">
+                      <h3>HABEENKA ABWAANKA</h3>
+                      <P>Lorem ipsum ,  Fugit, minus quas a, dolorem 
+                          p reiciendis quaerat neque ea</P>   
+                    </div>
+
+                    <div class="habeenka-abwaanka">
+                        <h3>HABEENKA ABWAANKA</h3>
+                        <P>Lorem ipsum ,  Fugit, minus quas a, dolorem 
+                            p reiciendis quaerat neque ea</P>   
+                      </div>
+
+                      <div class="habeenka-abwaanka">
+                        <h3>HABEENKA ABWAANKA</h3>
+                        <P>Lorem ipsum ,  Fugit, minus quas a, dolorem 
+                            p reiciendis quaerat neque ea</P>   
+                      </div>
+
+                      <div class="habeenka-abwaanka">
+                        <h3>HABEENKA ABWAANKA</h3>
+                        <P>Lorem ipsum ,  Fugit, minus quas a, dolorem 
+                            p reiciendis quaerat neque ea</P>   
+                      </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="arrow">
+       <div class="arrow-container">
+           <i class="fa fa-chevron-down"></i>
+       </div>
+    </section>
+
+    <section class="shuuriye-section">
+        <div class="shuuriye-container">
+          <img src="images/shuuriye.png">
+        </div>
+    </section>
+
+     <section class="tabs-section">
+    <div class="tabs_menu_container">
+       <ul class="tab_navigation">
+          <li>TAARIIKHDA</li>
+          <li class="wax">WAXAYAABAHA DHALIYAY</li>
+          <li class="ujed">UJEEDADA</li>
+          <li class="shaqo">SHAQADA XARUNTA</li>
+       </ul>
+
+       <div class="tab_container_area">
+           <div class="tab_container">
+              <p>TAARIIKHDA Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+              UJEEDADA Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pa
+          .</p>
+           </div>
+
+            <div class="tab_container">
+              <p>WAXYAABAHA DHALIYAY Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+           </div>
+
+            <div class="tab_container">
+              <p>UJEEDADA Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+           </div>
+
+            <div class="tab_container">
+              <p>SHAQADA XARUNTA Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+           </div>
+       </div>
+    </div>
+
+    <div class="form-container">
+        <div class="form">
+        <h1>RUKUMO</h1>
+       <form>
+        <div class="input-group">
+         <label>Name</label>
+         <input type="text" name="">
+        </div>
+
+        <div class="input-group">
+         <label>Email</label>
+         <input type="text" name="">
+        </div>
+
+        <button class="dir">DIR</button>
+       </form>
+   </div>
+    </div>
+  </section>
+
+  <section class="barnaamijyada-section">
+      <div class="barnaamijyada-container">
+        <img src="images/barnaamijyada.png">
+      </div>
+  </section>
+
+  <section class="games-section">
+   <div class="games-container">
+     <div class="games-box ciyaar">
+        <h2>CIYAAR DHAQAMEED</h2>
+     </div>
+
+     <div class="games-box habeenka">
+        <h2>HABEENKA ABWAANKA</h2>
+     </div>
+
+     <div class="games-box shaah">
+        <h2>SHAAH IYO SHEEKO</h2>
+     </div>
+   </div>
+</section>
+
+
+<!--- FOOOTER SECTION---->
+<section class="footer-section">
+	<div class="footer-container">
+   <div class="footer-logo">
+      <img src="images/logo.png">
+   </div>
+
+   <div class="footer-copywrite">
+       <p>&copyXARUNTA DHAQANKA IYO SUUGAANTA</p>
+   </div>
+
+   <div class="footer-social">
+      <ul>
+        <li><a href=""><i class="fa fa-facebook"></i></a></li>
+        <li><a href=""><i class="fa fa-twitter"></i></a></li>
+        <li><a href=""><i class="fa fa-instagram"></i></a></li>
+     </ul>
+   </div>
+</div>
+</section>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
